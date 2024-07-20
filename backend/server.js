86bf74c4b3d4a6be0d1ce1538aecb217384f8611
Stephanie Marie <fullstackwebdevsteph@gmail.com>
2024-07-20 13:35:31 -0300

    cors update

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,13 @@ const PORT = process.env.PORT || 5050;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
