0fb56c949f5c173015ee0e139a3d34d3c3d832fb
Stephanie Marie <fullstackwebdevsteph@gmail.com>
2024-07-31 18:20:16 -0300

    delete extra js file, remove blogpost comments, update about me text

--- client/src/Components/AboutMe/AboutMe.jsx
+++ client/src/Components/AboutMe/AboutMe.jsx
@@ -7,9 +7,20 @@ const AboutMe = () => {
         <div className="sidebar__section">
             <h3>About:</h3>
             <img src={AboutImg} alt="About Me Image" className="about-me__image" />
+
+
             <p className="sidebar__about-text">
-                With a passion for creative outlets—whether that's playing piano, ukulele, belting impromptu lyrics from the heart, or dancing like T. Swift (no shade, girl!)—being with animals, getting lost in nature with an instrumental soundtrack playing in the pods to set the mood, meal prepping, lifting, learning about skincare, or slowly developing my blog—thanks Wix for the inspo! This is another creative idea I only wish I'd started sooner—many stories await! I'm a 34-year-old, blonde-haired, blue-eyed, size 4, single, kidless, manless, obsessed dog momma, wandering wildberry going where the wind takes me. From vegetable to victor... sort of. Join me for the next 77 years ahead! And yes, I plan on living until 111 🪄
+                With a passion for creative outlets—whether that's playing piano, ukulele, belting impromptu lyrics from the heart, or dancing like T. Swift (no shade, girl!)—being with animals, getting lost in nature with an instrumental soundtrack playing in the pods, meal prepping, lifting, learning about skincare, or slowly developing my blog—hey Wix, thanks for the UI inspo! This is yet another creative idea that I only wish I'd started sooner—many stories await!
             </p>
+            <p>
+                In a world where authenticity is nothing but a buzzword, filters cover the truth of most images, and most anything you find on the WWW is certainly iced with AI, welcome to my nest, a digital trash can that homes the "real."
+            </p>
+            <p>
+                I'm a 34-year-young, brownish-blonde-haired, blue-eyed, obsessed dog momma, going where the wind takes me. From vegetable to victor... sort of. Join me for the next 77 years ahead! And yes, I plan on living until 111 🪄
+            </p>
+
+
+
         </div>
     );
 };
--- hi.js
+++
@@ -1,2 +0,0 @@
-// for commits
-///PUSH
